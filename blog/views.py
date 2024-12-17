d35bdb12dcbe23b41c11b50e8a1eac96165289d0
Akkilesh-A <akkilalagar05@gmail.com>
2024-12-17 08:52:07 +0530

    feat: setup dynamic blog routes

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
+
+app_name="blog"
 
 # Create your views here.
 def all_blogs(request):
     blogs=Blog.objects.order_by('-date')[:3]
     return render(request,'blog/all_blogs.html',{'blogs':blogs})
+
+def detail(request,blog_id):
+    blog=get_object_or_404(Blog,pk=blog_id)
+    return render(request,'blog/detail.html',{'id':blog_id,'blog':blog})
